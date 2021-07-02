783db762429daf80bdf5011adaafb61d06f12f5b
Max Weber <max.weber@sonomotors.com>
2021-07-02 17:17:06 +0200

    First try of a solution

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -35,17 +34,28 @@ class _MyHomePageState extends State<MyHomePage> {
   Signaling signaling = Signaling();
   RTCVideoRenderer _localRenderer = RTCVideoRenderer();
   RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
+  String? roomId;
+  TextEditingController textEditingController = TextEditingController(text: '');
 
   @override
   void initState() {
-    super.initState();
-  }
+    _localRenderer.initialize();
+    _remoteRenderer.initialize();
 
-  @override
-  deactivate() {
-    super.deactivate();
-    _localRenderer.dispose();
-    _remoteRenderer.dispose();
+    signaling.onAddRemoteStream = ((stream) {
+      print("=============>>>===>>==>>>===>>>");
+      print(stream.active);
+      print(stream.id);
+      print(_remoteRenderer.renderVideo);
+      print(_remoteRenderer.srcObject?.getTracks().length);
+
+      print("=============>>>===>>==>>>===>>>");
+
+      _remoteRenderer.srcObject = stream;
+      setState((){});
+
+    });
+    super.initState();
   }
 
   @override
@@ -59,23 +69,81 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Welcome to Flutter Explained - WebRTC"),
       ),
       body: Column(
         children: [
+          SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  signaling.openUserMedia(_localRenderer, _remoteRenderer);
+                },
+                child: Text("Open camera & microphone"),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  roomId = await signaling.createRoom(_remoteRenderer);
+                  textEditingController.text = roomId!;
+                  setState(() {});
+                },
+                child: Text("Create room"),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Add roomId
+                  signaling.joinRoom(
+                    textEditingController.text,
+                    _remoteRenderer,
+                  );
+                },
+                child: Text("Join room"),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  signaling.hangUp(_localRenderer);
+                },
+                child: Text("Hangup"),
+              )
+            ],
+          ),
+          SizedBox(height: 8),
           Expanded(
             child: Row(
               children: [
-                Expanded(child: RTCVideoView(_localRenderer, mirror: true)),
+                SizedBox(
+                  child: RTCVideoView(_localRenderer, mirror: true),
+                  width: 200,
+                  height: 200,
+                ),
                 SizedBox(width: 10),
-                Expanded(child: RTCVideoView(_remoteRenderer, mirror: true)),
+                SizedBox(
+                  child: RTCVideoView(_remoteRenderer),
+                  width: 200,
+                  height: 200,
+                ),
               ],
             ),
           ),
           Expanded(
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [],
+            child: Column(
+              children: [
+                Text("Join the following Room: "),
+                TextFormField(
+                  controller: textEditingController,
+                )
+              ],
             ),
           )
         ],
--- lib/signaling.dart
+++ lib/signaling.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
+
+typedef void StreamStateCallback(MediaStream stream);
 
 class Signaling {
   Map<String, dynamic> configuration = {
@@ -17,9 +21,12 @@ class Signaling {
   MediaStream? localStream;
   MediaStream? remoteStream;
   String? roomId;
+  String? currentRoomText;
+  StreamStateCallback? onAddRemoteStream;
 
-  createRoom() async {
+  Future<String> createRoom(RTCVideoRenderer remoteRenderer) async {
     FirebaseFirestore db = FirebaseFirestore.instance;
+    DocumentReference roomRef = db.collection('rooms').doc();
 
     print('Create PeerConnection with configuration: $configuration');
 
@@ -27,15 +34,31 @@ class Signaling {
 
     registerPeerConnectionListeners();
 
-    // Add code for creating a room
-
     localStream?.getTracks().forEach((track) {
       peerConnection?.addTrack(track, localStream!);
     });
 
-    // Code for creating a room below
-
     // Code for collecting ICE candidates below
+    var callerCandidatesCollection = roomRef.collection('callerCandidates');
+
+    peerConnection?.onIceCandidate = (RTCIceCandidate candidate) {
+      print('Got candidate: ${candidate.toMap()}');
+      callerCandidatesCollection.add(candidate.toMap());
+    };
+    // Finish Code for collecting ICE candidate
+
+    // Add code for creating a room
+    RTCSessionDescription offer = await peerConnection!.createOffer();
+    await peerConnection!.setLocalDescription(offer);
+    print('Created offer: $offer');
+
+    Map<String, dynamic> roomWithOffer = {'offer': offer.toMap()};
+
+    await roomRef.set(roomWithOffer);
+    var roomId = roomRef.id;
+    print('New room created with SDK offer. Room ID: $roomId');
+    currentRoomText = 'Current room is $roomId - You are the caller!';
+    // Created a Room
 
     peerConnection?.onTrack = (RTCTrackEvent event) {
       print('Got remote track: ${event.streams[0]}');
@@ -46,19 +69,53 @@ class Signaling {
       });
     };
 
-    // listening for remote session description below
+    // Listening for remote session description below
+    roomRef.snapshots().listen((snapshot) async {
+      print('Got updated room: ${snapshot.data()}');
+
+      Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
+      if (peerConnection?.getRemoteDescription() != null &&
+          data['answer'] != null) {
+        var answer = RTCSessionDescription(
+          data['answer']['sdp'],
+          data['answer']['type'],
+        );
+
+        print("Someone tried to connect");
+        await peerConnection?.setRemoteDescription(answer);
+      }
+    });
+    // Listening for remote session description above
 
     // Listen for remote Ice candidates below
+    roomRef.collection('calleeCandidates').snapshots().listen((snapshot) {
+      snapshot.docChanges.forEach((change) {
+        if (change.type == DocumentChangeType.added) {
+          Map<String, dynamic> data = change.doc.data() as Map<String, dynamic>;
+          print('Got new remote ICE candidate: ${jsonEncode(data)}');
+          peerConnection!.addCandidate(
+            RTCIceCandidate(
+              data['candidate'],
+              data['sdpMid'],
+              data['sdpMLineIndex'],
+            ),
+          );
+        }
+      });
+    });
+    // Listen for remote ICE candidates above
+
+    return roomId;
   }
 
-  Future<void> joinRoom() async {
+  Future<void> joinRoom(String roomId, RTCVideoRenderer remoteVideo) async {
     // read roomid from textbox
     print('Join room $roomId');
     // set text with roomid in UI
-    await joinRoomById();
+    await joinRoomById(roomId, remoteVideo);
   }
 
-  joinRoomById() async {
+  joinRoomById(String roomId, RTCVideoRenderer remoteVideo) async {
     FirebaseFirestore db = FirebaseFirestore.instance;
     DocumentReference roomRef = db.collection('rooms').doc('$roomId');
     var roomSnapshot = await roomRef.get();
@@ -75,6 +132,16 @@ class Signaling {
       });
 
       // Code for collecting ICE candidates below
+      var calleeCandidatesCollection = roomRef.collection('calleeCandidates');
+      peerConnection!.onIceCandidate = (RTCIceCandidate candidate) {
+        if (candidate == null) {
+          print('onIceCandidate: complete!');
+          return;
+        }
+        print('onIceCandidate: ${candidate.toMap()}');
+        calleeCandidatesCollection.add(candidate.toMap());
+      };
+      // Code for collecting ICE candidate above
 
       peerConnection?.onTrack = (RTCTrackEvent event) {
         print('Got remote track: ${event.streams[0]}');
@@ -85,34 +152,66 @@ class Signaling {
       };
 
       // Code for creating SDP answer below
+      var data = roomSnapshot.data() as Map<String, dynamic>;
+      print('Got offer $data');
+      var offer = data['offer'];
+      await peerConnection?.setRemoteDescription(
+        RTCSessionDescription(offer['sdp'], offer['type']),
+      );
+      var answer = await peerConnection!.createAnswer();
+      print('Created Answer $answer');
+
+      Map<String, dynamic> roomWithAnswer = {
+        'answer': {'type': answer.type, 'sdp': answer.sdp}
+      };
+
+      await roomRef.update(roomWithAnswer);
+      // Finished creating SDP answer
 
       // Listening for remote ICE candidates below
+      roomRef.collection('callerCandidates').snapshots().listen((snapshot) {
+        snapshot.docChanges.forEach((document) {
+          var data = document.doc.data() as Map<String, dynamic>;
+          print(data);
+          print('Got new remote ICE candidate: $data');
+          peerConnection!.addCandidate(
+            RTCIceCandidate(
+              data['candidate'],
+              data['sdpMid'],
+              data['sdpMLineIndex'],
+            ),
+          );
+        });
+      });
     }
   }
 
   Future<void> openUserMedia(
-      event, RTCVideoRenderer localVideo, RTCVideoRenderer remoteVideo) async {
-    var stream = await navigator.mediaDevices.getUserMedia(
-      {'video': true, 'audio': true},
-    );
+      RTCVideoRenderer localVideo, RTCVideoRenderer remoteVideo) async {
+    var stream = await navigator.mediaDevices
+        .getUserMedia({'video': true, 'audio': true});
+
+    print("Hier");
 
     localVideo.srcObject = stream;
     localStream = stream;
 
-    remoteStream = await createLocalMediaStream('remote');
-    remoteVideo.srcObject = remoteStream;
+    print("Hier");
+
+    remoteVideo.srcObject = await createLocalMediaStream('key');
 
     print('Stream: ${localVideo.srcObject}');
   }
 
-  Future<void> hangUp(event, RTCVideoRenderer localVideo) async {
+  Future<void> hangUp(RTCVideoRenderer localVideo) async {
     List<MediaStreamTrack> tracks = localVideo.srcObject!.getTracks();
     tracks.forEach((track) {
       track.stop();
     });
 
-    if (remoteStream != null)
+    if (remoteStream != null) {
       remoteStream!.getTracks().forEach((track) => track.stop());
+    }
     if (peerConnection != null) peerConnection!.close();
 
     if (roomId != null) {
@@ -126,6 +225,9 @@ class Signaling {
 
       await roomRef.delete();
     }
+
+    localStream!.dispose();
+    remoteStream?.dispose();
   }
 
   void registerPeerConnectionListeners() {
@@ -143,6 +245,12 @@ class Signaling {
 
     peerConnection?.onIceGatheringState = (RTCIceGatheringState state) {
       print('ICE connection state change: $state');
+    };
+
+    peerConnection?.onAddStream = (MediaStream stream) {
+      print("Add remote stream");
+      onAddRemoteStream?.call(stream);
+      remoteStream = stream;
     };
   }
 }
